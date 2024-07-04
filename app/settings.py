9788652c4cffa11bf92305e6a0443cbc4ecf9b5d
thiago araujo <devthiagoaraujo@gmail.com>
2024-07-03 21:55:42 -0300

    feat: app modeling

--- app/settings.py
+++ app/settings.py
@@ -37,6 +37,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'brands',
+    'categories',
+    'suppliers',
+    'products',
+    'inflows',
+    'outflows',
 ]
 
 MIDDLEWARE = [
