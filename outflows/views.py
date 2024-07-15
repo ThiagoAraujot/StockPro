12b75a675692a0020106126350333ac7082a9786
thiago araujo <devthiagoaraujo@gmail.com>
2024-07-14 22:12:07 -0300

    feat: Outflow page metrics

--- outflows/views.py
+++ outflows/views.py
@@ -1,7 +1,7 @@
 from django.urls import reverse_lazy
 from django.views.generic import ListView, CreateView, DetailView
 from . import models, forms
-# Create your views here.
+from app import metrics
 
 
 class OutflowListView(ListView):
@@ -18,6 +18,13 @@ class OutflowListView(ListView):
             queryset = queryset.filter(product__title__icontains=product)
 
         return queryset
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        context['sales_metrics'] = metrics.get_sales_metrics()
+
+        return context
 
 
 class OutflowCreateView(CreateView):
